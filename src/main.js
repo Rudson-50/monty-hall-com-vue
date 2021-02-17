a80e3654708462644dfb47897e189fd71ad50932
Rudson Gabriel <rudsongabriel1@outlook.com>
2021-02-17 12:42:38 -0300

    Monty Hall Com vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
+import App from './App'
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue ({
+    render: h => h(App)
+}).$mount("#app")
